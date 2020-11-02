ef8935511ce1dd58252f2821fe75d07ea667c054
drimix001 <69410679+drimix001@users.noreply.github.com>
2020-11-02 18:39:34 +0200

    Updates

---
+++ precache-manifest.d7ee8c18bc4fb8aed10c97547f7daa1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff12a1b87d31c380c461448d7001552",
+    "url": "/depcalc/index.html"
+  },
+  {
+    "revision": "dddbec80d810108344cc",
+    "url": "/depcalc/static/css/main.27353e52.chunk.css"
+  },
+  {
+    "revision": "cf7a3e1b6b39279f2684",
+    "url": "/depcalc/static/js/2.99ade0b4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/depcalc/static/js/2.99ade0b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dddbec80d810108344cc",
+    "url": "/depcalc/static/js/main.25d393b0.chunk.js"
+  },
+  {
+    "revision": "66d2d67a14a6857850c1",
+    "url": "/depcalc/static/js/runtime-main.0def2000.js"
+  }
+]);
